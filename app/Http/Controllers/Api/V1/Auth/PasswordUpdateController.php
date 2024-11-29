569cf84cccd2bb23acacb6efb6901fecf6719b0c
= <=>
2024-11-29 17:00:31 +0100

    Added User Password update

---
+++ app/Http/Controllers/Api/V1/Auth/PasswordUpdateController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserPasswordRequest;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
+
+class PasswordUpdateController extends Controller
+{
+    //
+    public function __invoke(UserPasswordRequest $request): JsonResponse
+    {
+        //
+        auth()->user()->update([
+            'password' => Hash::make($request->validated('password')),
+        ]);
+        //
+        return response()->json([
+            'message' => 'Password updated successfully'
+        ], Response::HTTP_ACCEPTED);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\V1\Auth\RegisterController;
 use App\Http\Controllers\Api\V1\Auth\LoginController;
 use App\Http\Controllers\Api\V1\Auth\ProfileController;
+use App\Http\Controllers\Api\V1\Auth\PasswordUpdateController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -17,5 +18,7 @@ Route::prefix("auth")->group(function () {
     Route::middleware('auth:sanctum')->group(function () {
         Route::get('profile', action: [ProfileController::class, 'show']);
         Route::put('profile', action: [ProfileController::class, 'update']);
+        // password update
+        Route::put('password', PasswordUpdateController::class);
     });
 });
