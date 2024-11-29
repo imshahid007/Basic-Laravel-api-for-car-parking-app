acb0a9015d748e4ee3c916a5eeedf9f7fa79f8f3
= <=>
2024-11-29 15:33:02 +0100

    Added User Login

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,15 @@
 <?php
 
 use App\Http\Controllers\Api\V1\Auth\RegisterController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 
 //
 Route::prefix("auth")->group(function () {
     Route::post("register", RegisterController::class);
+    //
+    Route::post('login', LoginController::class);
+
+
 });
